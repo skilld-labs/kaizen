d45108c591dc88431bcf8576099c80b9ae922c31
HOG <hogseruj1@gmail.com>
2019-01-02 14:08:47 +0200

    Edited preview.js added css-vars-ponyfill.

--- color/preview.js
+++ color/preview.js
@@ -3,17 +3,17 @@
  * This is simple js for management of color module display.
  */
 
-(function ($, Drupal, drupalSettings) {
+(function (Drupal) {
   Drupal.color = {
     logoChanged: false,
     callback: function callback(context, settings, $form) {
+      var $colorPreview = $form[0].querySelectorAll('.color-preview .brands__item > div:first-of-type');
+      var $colorPalette = $form[0].querySelectorAll('.js-color-palette');
 
-      var $colorPreview = $form.find('.color-preview .brands__item > div:first-of-type');
-      var $colorPalette = $form.find('.js-color-palette');
-
-      $.each($colorPreview, function () {
-        $(this).css('backgroundColor', $colorPalette.find('input[name="palette[' + $(this).attr('class') + ']"]').val());
-      });
+      for (var i = 0; i < $colorPreview.length; i++) {
+        $colorPreview[i].style.backgroundColor = $colorPalette[0].querySelectorAll('input[name="palette[' +
+          $colorPreview[i].getAttribute('class') + ']"]')[0].value
+      }
     }
   };
-})(jQuery, Drupal, drupalSettings);
+})(Drupal);
--- src/js/init.js
+++ src/js/init.js
@@ -1,6 +1,8 @@
 import './.modernizrrc';
 import {initBreakpointsCssReload} from './breakpoints';
+import cssVars from 'css-vars-ponyfill';
 
 // TODO: Check if css classes need to be reloaded by default.
 // https://i.gyazo.com/67cc358aca4e7216e587a740b9f96b99.gif
 initBreakpointsCssReload();
+cssVars();
