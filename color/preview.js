d07f2dba2f6a087194c1ca7a2d1af1c4f1c042da
Sergiy Borodulin <hogseruj1@gmail.com>
2019-01-11 17:09:28 +0200

    Added color palette. (#13)
    * Added color palette.
    
    * Edited preview.js added css-vars-ponyfill.
    
    * Edited colors styles and scripts.

---
+++ color/preview.js
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * This is simple js for management of color module display.
+ */
+
+(function (Drupal) {
+  Drupal.color = {
+    logoChanged: false,
+    callback: function callback(context, settings, $form) {
+      var $colorPreview = $form[0].querySelectorAll('.color-preview .colors__item > div:first-of-type');
+      var $colorPalette = $form[0].querySelectorAll('.js-color-palette');
+
+      for (var i = 0; i < $colorPreview.length; i++) {
+        $colorPreview[i].style.backgroundColor = $colorPalette[0].querySelectorAll('input[name="palette[' +
+          $colorPreview[i].getAttribute('class') + ']"]')[0].value
+      }
+    }
+  };
+})(Drupal);
--- src/js/init.js
+++ src/js/init.js
@@ -1,6 +1,11 @@
-import './.modernizrrc';
+import modernizr from './.modernizrrc';
 import {initBreakpointsCssReload} from './breakpoints';
+import cssVars from 'css-vars-ponyfill';
 
 // TODO: Check if css classes need to be reloaded by default.
 // https://i.gyazo.com/67cc358aca4e7216e587a740b9f96b99.gif
 initBreakpointsCssReload();
+// Add css-vars-ponyfill if it's not support.
+if (!modernizr.testAllProps('customproperties')) {
+  cssVars();
+}
