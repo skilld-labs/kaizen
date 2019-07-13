db0a13ed375a565977888d9c342857f0d04abd11
iberdinsky-skilld <iberdinsky@mail.skilld.fr>
2019-07-13 14:21:05 +0600

    added logo and user menu.

---
+++ STARTERKIT/primary/packages/primary-components/atoms/user-menu/user-menu.stories.js
@@ -0,0 +1,14 @@
+import { storiesOf } from '@storybook/html';
+import './user-menu.css';
+
+
+const template = require('./a-user-menu.html.twig');
+const data = require('./a-user-menu.json');
+
+// Uncomment next 2 lines if your templates contains {{ attibutes.addClass(...) }} or similar logic.
+// import drupalAttribute from 'drupal-attribute';
+// data.attributes = new drupalAttribute();
+
+
+storiesOf('atoms|user-menu', module).add('default', () => template(data));
+
