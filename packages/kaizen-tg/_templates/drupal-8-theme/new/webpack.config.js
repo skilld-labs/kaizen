9e32edbabe3e0aeb828268ad474a920d2c855529
iberdinsky-skilld <iberdinsky@mail.skilld.fr>
2022-03-25 12:51:20 +0600

    Url option

--- packages/kaizen-tg/_templates/drupal-8-theme/new/webpack.config.js
+++ packages/kaizen-tg/_templates/drupal-8-theme/new/webpack.config.js
@@ -64,6 +64,7 @@ module.exports = {
             loader: 'css-loader',
             options: {
               sourceMap: process.env.NODE_ENV === 'development',
+              url: false,
             },
           },
           {
